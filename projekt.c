1950da509727b641b05857fdff43fb4935ac73bb
fsk85 <151266689+fsk85@users.noreply.github.com>
2024-04-12 02:06:34 +0200

    Add files via upload

--- projekt.c
+++ projekt.c
@@ -1,160 +1,193 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <linux/fs.h>
-#include <stdbool.h>
-#include <dirent.h>
-#include <string.h>
 #include <linux/limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
-
-
+#include <unistd.h>
 
 /* todo:
  * zmienic perrory na logi, bo stdout jest zamkniete
+ * dodac funkcje copy co wybiera czy copy big czy copy small i dac ja w syncNonRecursive
  */
-typedef struct fileList{
-  char fileName[PATH_MAX];
-  off_t fileSize;
-  time_t modDate; /* nie wiem czy to sie przyda */
-  mode_t permissions;
-  struct fileList *next;
+typedef struct fileList
+{
+    char fileName[PATH_MAX];
+    off_t fileSize;
+    time_t modDate; 
+    mode_t permissions;
+    struct fileList* next;
 } fileList;
 
-typedef struct{
-  int sleep_time;
-  bool recursive;
-  int threshold;
+typedef struct
+{
+    int sleep_time;
+    bool recursive;
+    int threshold;
 } config;
 
-typedef struct subDirList{
-  char path[PATH_MAX];
-  struct subDirList *next;
-}subDirList;
+typedef struct subDirList
+{
+    char path[PATH_MAX];
+    mode_t permissions;
+    struct subDirList* next;
+} subDirList;
 
-config flags = {300, false, 10000};
+config flags = { 300, false, 10000 };
 
 #define NR_OPEN 1024
 
+/* funkcja ktora sprawdza czy plik z jednego katalogu istnieje i jest taki sam
+ * jak w drugim katalogu*/
 
-/* funkcja ktora sprawdza czy plik z jednego katalogu istnieje i jest taki sam jak w drugim katalogu*/
-
-int changedFile(fileList *sourceNode, fileList *targetNode){
-    if(!sourceNode) return 0;
-    printf("sprawdzanie: %s | %d | %o\n",sourceNode->fileName,sourceNode->fileSize,sourceNode->permissions);
-    int changed=1;
-    while(targetNode){
-    if(!strcmp(sourceNode->fileName, targetNode->fileName) && sourceNode->fileSize == targetNode->fileSize && sourceNode->permissions == targetNode->permissions && sourceNode->modDate < targetNode ->modDate){ 
-    changed=0;
-    continue;
+int
+changedFile(fileList* sourceNode, fileList* targetNode)
+{
+    if (!sourceNode)
+        return 0;
+    printf("sprawdzanie: %s | %d | %o\n",
+           sourceNode->fileName,
+           sourceNode->fileSize,
+           sourceNode->permissions);
+    int changed = 1;
+    while (targetNode) {
+        if (!strcmp(sourceNode->fileName, targetNode->fileName) &&
+            sourceNode->fileSize == targetNode->fileSize &&
+            sourceNode->permissions == targetNode->permissions &&
+            sourceNode->modDate < targetNode->modDate) {
+            changed = 0;
+            continue;
+        }
+        targetNode = targetNode->next;
     }
-    targetNode = targetNode->next;
-  }
-  return changed;
-} 
-
-void addToList(fileList **head, char *filename, off_t filesize, time_t moddate, mode_t permissions){
-  fileList *node = malloc(sizeof(fileList));
-  if(!node){
-    perror("malloc");
-    exit(EXIT_FAILURE);
-  }
-  strcpy(node->fileName, filename);
-  node->fileSize = filesize;
-  node->modDate = moddate;
-  node->permissions = permissions;
-  node->next = NULL;
-  if(*head == NULL){
-    *head = node;
-    return;
-  }
-  fileList *last = *head;
-  while(last->next != NULL)
-  {
-    last=last->next;
-  }
-  last->next = node; 
+    return changed;
 }
 
-fileList *saveFilesToList(char *dirPath){
-  /* inicjalizujemy odnosnik na pierwszy element w liscie plikow na NULL */
-  fileList *head=NULL;
-  /* otwieramy katalog */
-  DIR *dir = opendir(dirPath);
-  if(!dir){
-    perror("opendir");
-    exit(EXIT_FAILURE);
-  }
-  struct dirent *dirContent;
-  while((dirContent = readdir(dir)) != NULL){
-    /* pomijamy odnosnik . na katalog obecny i katalog poprzedni .. */
-    if (strcmp(dirContent->d_name, ".") == 0 || strcmp(dirContent->d_name, "..") == 0)
-      continue;
-    char fullPath[PATH_MAX];
-    struct stat info;
-    snprintf(fullPath, sizeof(fullPath), "%s/%s", dirPath, dirContent->d_name);
-    if (stat(fullPath, &info) == -1) {
-        perror("stat");
-        closedir(dir);
+void
+addToList(fileList** head,
+          char* filename,
+          off_t filesize,
+          time_t moddate,
+          mode_t permissions)
+{
+    fileList* node = malloc(sizeof(fileList));
+    if (!node) {
+        perror("malloc");
         exit(EXIT_FAILURE);
     }
-
-    /* Jezeli jest plikiem regularnym to dodajemy go do listy */
-    if(S_ISREG(info.st_mode)){
-      addToList(&head, dirContent->d_name, info.st_size, info.st_mtime,info.st_mode);
+    strcpy(node->fileName, filename);
+    node->fileSize = filesize;
+    node->modDate = moddate;
+    node->permissions = permissions;
+    node->next = NULL;
+    if (*head == NULL) {
+        *head = node;
+        return;
     }
-  }
-
-  return head;
+    fileList* last = *head;
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = node;
 }
 
-void checkDirs(char *sourceDir,char *targetDir)
+fileList*
+saveFilesToList(char* dirPath)
 {
-  DIR *srcDir;
-  DIR *targDir;
-  srcDir = opendir(sourceDir);
-  targDir = opendir(targetDir);
-  /* sprawdzamy czy mozna otworzyc katalogi */
-  if(srcDir == NULL || targDir == NULL){
-    perror("opendir");
-    exit(EXIT_FAILURE);
-  }
-  /* zamykamy katalogi, jesli wystapil blad to */
-  if(closedir(srcDir) == -1 || closedir(targDir) == -1){
-    perror("closedir");
-    exit(EXIT_FAILURE);
-  }
+    /* inicjalizujemy odnosnik na pierwszy element w liscie plikow na NULL */
+    fileList* head = NULL;
+    /* otwieramy katalog */
+    DIR* dir = opendir(dirPath);
+    if (!dir) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+    struct dirent* dirContent;
+    while ((dirContent = readdir(dir)) != NULL) {
+        /* pomijamy odnosnik . na katalog obecny i katalog poprzedni .. */
+        if (strcmp(dirContent->d_name, ".") == 0 ||
+            strcmp(dirContent->d_name, "..") == 0)
+            continue;
+        char fullPath[PATH_MAX];
+        struct stat info;
+        snprintf(
+          fullPath, sizeof(fullPath), "%s/%s", dirPath, dirContent->d_name);
+        if (stat(fullPath, &info) == -1) {
+            perror("stat");
+            closedir(dir);
+            exit(EXIT_FAILURE);
+        }
+
+        /* Jezeli jest plikiem regularnym to dodajemy go do listy */
+        if (S_ISREG(info.st_mode)) {
+            addToList(&head,
+                      dirContent->d_name,
+                      info.st_size,
+                      info.st_mtime,
+                      info.st_mode);
+        }
+    }
+
+    return head;
 }
 
-void appendSubDirList(subDirList **head, char *Path) {
-    subDirList *node = malloc(sizeof(subDirList));
-    if (!node) exit(EXIT_FAILURE);
+void
+checkDirs(char* sourceDir, char* targetDir)
+{
+    DIR* srcDir;
+    DIR* targDir;
+    srcDir = opendir(sourceDir);
+    targDir = opendir(targetDir);
+    /* sprawdzamy czy mozna otworzyc katalogi */
+    if (srcDir == NULL || targDir == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+    /* zamykamy katalogi, jesli wystapil blad to */
+    if (closedir(srcDir) == -1 || closedir(targDir) == -1) {
+        perror("closedir");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void
+appendSubDirList(subDirList** head, char* Path)
+{
+    subDirList* node = malloc(sizeof(subDirList));
+    if (!node)
+        exit(EXIT_FAILURE);
     strcpy(node->path, Path);
     node->next = NULL;
     if (!*head) {
         *head = node;
         return;
     }
-    subDirList *tmp = *head;
+    subDirList* tmp = *head;
     while (tmp->next) {
         tmp = tmp->next;
     }
     tmp->next = node;
 }
 
-subDirList *getSubDirs(subDirList *head, char *dirPath) {
-    DIR *dir;
+subDirList*
+getSubDirs(subDirList* head, char* dirPath)
+{
+    DIR* dir;
     dir = opendir(dirPath);
-    if (!dir) exit(EXIT_FAILURE);
+    if (!dir)
+        exit(EXIT_FAILURE);
 
-    struct dirent *subDir;
+    struct dirent* subDir;
     subDir = readdir(dir);
     appendSubDirList(&head, dirPath);
     while (subDir != NULL) {
-        if (subDir->d_type == DT_DIR && strcmp(subDir->d_name, ".") != 0 && strcmp(subDir->d_name, "..") != 0) {
+        if (subDir->d_type == DT_DIR && strcmp(subDir->d_name, ".") != 0 &&
+            strcmp(subDir->d_name, "..") != 0) {
             char subDirPath[PATH_MAX] = { 0 };
             strcat(subDirPath, dirPath);
             strcat(subDirPath, "/");
@@ -168,116 +201,177 @@ subDirList *getSubDirs(subDirList *head, char *dirPath) {
     return head;
 }
 
-void daemonLoop(char *sourceDir, char *targetDir) 
+
+char* getRelativePath(const char *basePath, const char *targetPath) {
+    size_t baseLen = strlen(basePath);
+    size_t targetLen = strlen(targetPath);
+
+    if (strncmp(basePath, targetPath, baseLen) != 0) {
+        printf("Error: '%s' is not a subdirectory of '%s'\n", targetPath, basePath);
+        return NULL;
+    }
+
+    const char *relativePath = targetPath + baseLen;
+
+    if (relativePath[0] == '/' || relativePath[0] == '\\') {
+        relativePath++;
+    }
+
+    return strdup(relativePath);
+}
+
+void syncNonRecursive(char *sourceDirPath, char *targetDirPath){
+      fileList* srcDirHead = saveFilesToList(sourceDirPath);
+      fileList* targetDirHead = saveFilesToList(targetDirPath);
+      fileList* node = srcDirHead;
+      fileList* nodeTarg = targetDirHead;
+        while (node) {
+            /* Sprawdzamy czy plik istnieje w katalogu docelowym i czy
+            * zostal zmieniony */
+           if (changedFile(node, targetDirHead)) {
+               /*Skopiuj plik do katalogu docelowego */
+                    printf("zmienil sie lub nie istnieje: %s\n",
+                           node->fileName);
+                }
+                node = node->next;
+            }
+}
+
+void
+syncRecursive(char *sourceDirPath, char *targetDirPath)
 {
-  while(true){
-    if (flags.recursive == false){
-    fileList *srcDirHead = saveFilesToList(sourceDir);
-    fileList *targetDirHead = saveFilesToList(targetDir);
-    fileList *node = srcDirHead;
-    fileList *nodeTarg = targetDirHead;
-    while(node){
-    /* Sprawdzamy czy plik istnieje w katalogu docelowym i czy zostal zmieniony */
-      if(changedFile(node,targetDirHead)){
-        /*Skopiuj plik do katalogu docelowego */
-        printf("zmienil sie lub nie istnieje: %s\n",node->fileName);
-      }
-      node = node->next;
+    /* inicjalizujemy liste podkatalogow w katalogu zrodlowym*/
+    subDirList* srcDirHead = NULL;
+    srcDirHead = getSubDirs(srcDirHead, sourceDirPath);
+    printf("return1: targetDir: %s\n", targetDirPath);
+    /* inicjalizujemy liste podkatalogow w katalogu docelowym */
+    subDirList* targetDirHead = NULL;
+    targetDirHead = getSubDirs(targetDirHead, targetDirPath);
+    printf("return2\n");
+    /* inicjalizujemy pomocnicze wskazniki na pierwszy element listy
+     * podkatalogow */
+    subDirList* srcDirNode = srcDirHead;
+    subDirList* targetDirNode = targetDirHead;
+    
+    while(srcDirNode){
+    char *relativePath = getRelativePath(sourceDirPath,srcDirNode->path);
+    printf("RELATIVE: %s\n",relativePath);
+    char fullTargetPath[2046];
+    snprintf(fullTargetPath, sizeof(fullTargetPath), "%s/%s", targetDirPath, relativePath);
+    DIR *dir = opendir(fullTargetPath);
+    if(dir != NULL){
+      printf("istnieje KATALOG: %s\n", fullTargetPath);
+      closedir(dir);
+      syncNonRecursive(srcDirNode->path,fullTargetPath);
     }
-    /* todo: usun pliki ktore istnieja w katalogu docelowym, a nie istnieja w zrodlowym */
+    else{
+        printf("nie istnieje KATALOG: %s\n", fullTargetPath);
+        closedir(dir);
+        struct stat info;
+        stat(srcDirNode->path,&info);
+        mkdir(fullTargetPath,info.st_mode);
+        syncNonRecursive(srcDirNode->path,fullTargetPath);
     }
-    else {
-      /* inicjalizujemy liste podkatalogow w katalogu zrodlowym*/
-      subDirList *srcDirHead = NULL; 
-      srcDirHead = getSubDirs(srcDirHead,sourceDir);
-      printf("return1: targetDir: %s\n",targetDir);
-      /* inicjalizujemy liste podkatalogow w katalogu docelowym */
-      subDirList *targetDirHead = NULL; 
-      targetDirHead = getSubDirs(targetDirHead,targetDir);
-      printf("return2\n");
-      /* inicjalizujemy pomocnicze wskazniki na pierwszy element listy podkatalogow */
-      subDirList *srcDirNode = srcDirHead;
-      subDirList *targetDirNode = targetDirHead;
-      /* dopoki nie doszlismy do konca listy katalogow to porownujemy pliki */
-      
-      
-      
-    }
-    sleep(flags.sleep_time);
-      
-    /* todo: */
+    srcDirNode = srcDirNode -> next;
   }
+ 
 }
 
-
-int runDaemon(char *sourceDir, char *targetDir)
+void
+daemonLoop(char* sourceDir, char* targetDir)
 {
-  pid_t pid;
-  int i;
-
-  /* Tworzenie nowego procesu */  
-  pid = fork();
-  if (pid == -1)
-    return -1;
-  else if (pid != 0)
-      exit(EXIT_SUCCESS);
-  /*Stworzenie nowej sesji i grupy procesow*/ 
-  if (setsid() == -1)
-    return -1;
-
-  /*Ustawienie katalogu roboczego na katalog glowny*/ 
-  if (chdir("/") == -1)
-    return -1;
-  
-  pid_t cpid = getpid();
-  printf("sourceDir: %s targetDir: %s pid: %d t: %d p: %d\n",sourceDir,targetDir,cpid,flags.sleep_time,flags.threshold); 
-  /* Zamkniecie otwartych plikow */ 
-//  for (i = 0; i < NR_OPEN; i++)
- //   close(i);
-
-  /* Przeadresowanie deskryptorow plikow 0,1,2 na /dev/null */ 
- // open("/dev/null",O_RDWR);
- // dup(0);
- // dup(0);
-
-  daemonLoop(sourceDir,targetDir);
-  return 1;
+    while (true) {
+        if (flags.recursive == false) {
+            syncNonRecursive(sourceDir,targetDir);
+            /* todo: usun pliki ktore istnieja w katalogu docelowym, a nie
+             * istnieja w zrodlowym */
+            
+        } else {
+            syncRecursive(sourceDir,targetDir);
+        }
+        /* uwolnic pamiec */
+        sleep(flags.sleep_time);
+    }
+    /* todo: */
 }
 
-int main(int argc, char **argv)
+int
+runDaemon(char* sourceDir, char* targetDir)
 {
-  /* parsowanie argumentow */
-  int opt;
-  while((opt = getopt(argc,argv,"Rp:t:")) != -1)
-  {
-    switch(opt){
-      case 't':
-        flags.sleep_time = atoi(optarg);
-        break;
-      case 'R':
-        flags.recursive = true;
-        break;
-      case 'p':
-        flags.threshold = atoi(optarg); 
-        break;
-      default:
-        fprintf(stderr,"Sposob uzycia: %s [flagi] [katalog_zrodlowy] [katalog_docelowy]",argv[0]);
+    pid_t pid;
+    int i;
+
+    /* Tworzenie nowego procesu */
+    pid = fork();
+    if (pid == -1)
+        return -1;
+    else if (pid != 0)
+        exit(EXIT_SUCCESS);
+    /*Stworzenie nowej sesji i grupy procesow*/
+    if (setsid() == -1)
+        return -1;
+
+    /*Ustawienie katalogu roboczego na katalog glowny*/
+    if (chdir("/") == -1)
+        return -1;
+
+    pid_t cpid = getpid();
+    printf("sourceDir: %s targetDir: %s pid: %d t: %d p: %d\n",
+           sourceDir,
+           targetDir,
+           cpid,
+           flags.sleep_time,
+           flags.threshold);
+    /* Zamkniecie otwartych plikow */
+    //  for (i = 0; i < NR_OPEN; i++)
+    //   close(i);
+
+    /* Przeadresowanie deskryptorow plikow 0,1,2 na /dev/null */
+    // open("/dev/null",O_RDWR);
+    // dup(0);
+    // dup(0);
+
+    daemonLoop(sourceDir, targetDir);
+    return 1;
+}
+
+int
+main(int argc, char** argv)
+{
+    /* parsowanie argumentow */
+    int opt;
+    while ((opt = getopt(argc, argv, "Rp:t:")) != -1) {
+        switch (opt) {
+            case 't':
+                flags.sleep_time = atoi(optarg);
+                break;
+            case 'R':
+                flags.recursive = true;
+                break;
+            case 'p':
+                flags.threshold = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr,
+                        "Sposob uzycia: %s [flagi] [katalog_zrodlowy] "
+                        "[katalog_docelowy]",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    char* sourceDir = argv[optind];
+    char* targetDir = argv[optind + 1];
+    if (argv[optind + 2] != NULL) {
+        fprintf(stderr, "Podano niewlasciwa liczbe argumentow!\n");
         exit(EXIT_FAILURE);
     }
-  }
-     
-    char *sourceDir = argv[optind];
-    char *targetDir = argv[optind+1];
-    if(argv[optind+2] != NULL){
-      fprintf(stderr,"Podano niewlasciwa liczbe argumentow!\n");
-      exit(EXIT_FAILURE);
-  }
-  /* sprawdzamy czy podane katalogi istnieja, czy mamy do nich dostep.. */
-  checkDirs(sourceDir,targetDir); 
-  char sourceDirPath[PATH_MAX];
-  char targetDirPath[PATH_MAX];
-  realpath(sourceDir,sourceDirPath);
-  realpath(targetDir,targetDirPath);
-   
-  runDaemon(sourceDirPath,targetDirPath);
+    /* sprawdzamy czy podane katalogi istnieja, czy mamy do nich dostep.. */
+    checkDirs(sourceDir, targetDir);
+    char sourceDirPath[PATH_MAX];
+    char targetDirPath[PATH_MAX];
+    realpath(sourceDir, sourceDirPath);
+    realpath(targetDir, targetDirPath);
+
+    runDaemon(sourceDirPath, targetDirPath);
 }
